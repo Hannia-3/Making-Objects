a4d20abf3702186f8137360d7c66c6efe241793c
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-29 22:17:24 -0500

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,37 @@
+public class Vehicle {
+    protected int speed;
+    private double fuel;
+
+    public Vehicle(int speed, double fuel) {
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+
+    public void start() {
+        System.out.println("Vehicle is starting");
+    }
+
+    public void stop() {
+        System.out.println("Vehicle is stopping");
+    }
+
+    public final void displayFuel() {
+        System.out.println("Fuel level: " + this.fuel + " units");
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+}
